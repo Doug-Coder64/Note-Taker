63a376e40e546876b2c956bd20ce7531d9a81a99
Doug Peterson <douglaspeterson6@gmail.com>
2021-10-24 14:05:07 -0500

    Added get responses for webpage requests

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const { db } = require('./db/db');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const { db } = require('./public/db/db');
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/notes.html'));
 });
 
