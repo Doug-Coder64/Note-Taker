f67b066a3137f0b5aa2cf6f5d8a9f3bf4d3622f2
Doug Peterson <douglaspeterson6@gmail.com>
2021-10-24 14:20:23 -0500

    fixed stylesheet and added get for api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const fs = require('fs');
 
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 const { db } = require('./public/db/db');
 
@@ -23,3 +25,14 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(JSON.parse(fs.readFileSync("./public/db/db.json", "utf8")));
+});
+
+app.post('/api/notes' , (req, res) => {
+    console.log('POST');
+});
+
+app.delete('/api/notes', (req, res) => {
+
+});
